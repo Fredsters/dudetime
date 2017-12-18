5c2a672fc30bae30f60aa60d801b59b10a68fe8d
Frederik Schmitt <fred182@gmx.de>
2017-12-18 13:04:47 +0100

    fix dependencies

--- startDB.bat
+++ startDB.bat
@@ -1,1 +1,1 @@
-START "runas /user:administrator" cmd /K "cd C:\Program Files\MongoDB\Server\3.4\bin & mongod.exe --dbpath C:\Users\fred1\dev\siapp\data"
+START "runas /user:administrator" cmd /K "cd C:\Program Files\MongoDB\Server\3.4\bin & mongod.exe --dbpath C:\Users\fred1\dev\dudetime\data"
