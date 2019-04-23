864ed67cc6547edb8ac995794959a97df66fe6df
Frederik Schmitt <frederik.schmitt@sap.com>
2019-04-23 09:18:00 +0200

    remove firebase config

--- react-dude/DudeTime/constants/network.js
+++ react-dude/DudeTime/constants/network.js
@@ -1,6 +1,4 @@
 export const root = "http://10.18.37.35:3000";
 export const clientId = "216116664350-8epb9jv6h7atu8c35n1v6r87uukc0lj0.apps.googleusercontent.com";
 
-export const firebaseConfig = {
 
-};
